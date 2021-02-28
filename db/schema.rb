efa4355be455db8bb9397aead7b99e1fe17bcbca
tenpadrummer <yoshiro.kato520@gmail.com>
2021-02-28 19:00:54 +0900

    edit css and view

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,69 +10,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_617_071_907) do
+ActiveRecord::Schema.define(version: 2019_06_17_071907) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'activities', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'action_id'
-    t.string 'action_type'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "activities", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "action_id"
+    t.string "action_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'answers', force: :cascade do |t|
-    t.integer 'word_id'
-    t.integer 'choice_id'
-    t.integer 'lesson_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "answers", force: :cascade do |t|
+    t.integer "word_id"
+    t.integer "choice_id"
+    t.integer "lesson_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'choices', force: :cascade do |t|
-    t.integer 'word_id'
-    t.string 'content'
-    t.boolean 'correct'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "choices", force: :cascade do |t|
+    t.integer "word_id"
+    t.string "content"
+    t.boolean "correct"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'lessons', force: :cascade do |t|
-    t.integer 'category_id'
-    t.integer 'user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "lessons", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'relationships', force: :cascade do |t|
-    t.integer 'follower_id'
-    t.integer 'followed_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'nickname'
-    t.string 'realname'
-    t.string 'email'
-    t.string 'password_digest'
-    t.boolean 'admin'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "nickname"
+    t.string "realname"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'words', force: :cascade do |t|
-    t.integer 'category_id'
-    t.string 'content'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "words", force: :cascade do |t|
+    t.integer "category_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
